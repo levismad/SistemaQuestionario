f84279e1288c5b822076fe22ad70b27bea209b80
Levi E <levi.esteves@gmail.com>
2015-11-19 17:34:57 -0200

    Adicioando Base Controller

---
+++ questionario/SQ.Web/Controllers/Base/BaseController.cs
@@ -0,0 +1,50 @@
+﻿using SQ.Dominio.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQ.Web.Controllers.Base
+{
+    [HandleError]
+    public class BaseController : Controller
+    {
+        protected MUsuario Usuario { get { return GetUsuarioSession(); } }
+        protected void SetUsuarioSession(MUsuario u)
+        {
+            Session["Usuario"] = u;
+        }
+        protected MUsuario GetUsuarioSession()
+        {
+            var session = HttpContext.Session.Contents["Usuario"] as MUsuario;
+            return session;
+        }
+        protected void FinalizaSessao()
+        {
+            HttpContext.Session.Abandon();
+        }
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            bool xmlRequest = false;
+            if (base.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                xmlRequest = true;
+            }
+            else
+            {
+                ViewBag.MessageError = filterContext.Exception.Message;
+                ViewBag.StackTrace = filterContext.Exception.StackTrace;
+                filterContext.ExceptionHandled = true;
+                //if (xmlRequest)
+                //    filterContext.Result = PartialView("Error");
+                //else
+                filterContext.Result = View("Error");
+
+            }
+
+
+        }
+    }
+
+}
---
+++ questionario/SQ.Web/Exceptions/ControllerException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQ.Web.Exceptions
+{
+    class ControllerException : Exception
+    {
+        public ControllerException() { }
+        public ControllerException(string message) : base(message) { }
+
+    }
+}
