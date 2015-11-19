19b5e02f50b3ab7a9e8a6c2d47120166cc242090
Levi E <levi.esteves@gmail.com>
2015-11-19 17:35:28 -0200

    Adicionado Login Controller

---
+++ questionario/SQ.Web/Controllers/Login/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using SQ.Dominio.Interface;
+using SQ.Repositorio.Interface;
+using SQ.Web.Controllers.Base;
+using SQ.Web.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQ.Web.Controllers.Login
+{
+    [LoginFilter]
+    public class LoginController : BaseController
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Acesso()
+        {
+            return View();
+        }
+
+        public ActionResult SessaoErrorAjax()
+        {
+            return View("ErroSessao");
+        }
+
+
+        public ActionResult SessaoError()
+        {
+            return View("ErroSessaoFull");
+        }
+
+        [HttpPost]
+        public ActionResult Acessar(string ebusiness, string senha)
+        {
+            MUsuario u = new DUsuario().ConsultarUsuario(ebusiness, senha);
+            //DNotificacoes notificicoesDAO = new DNotificacoes();
+            try
+            {
+
+                // var notificacoes = notificicoesDAO.ListarParcial(u);
+                // string popover = "";
+                // if (notificacoes.Count > 0)
+                // {
+                // notificacoes.ForEach(x =>
+                // {
+                // popover += "<div class='div-link-notificacao'><a
+                //href='#' class='link-notificacao' data-tipo='" + x.Tipo + "' data-data='" +
+                //x.Data + "' data-hora='" + x.Hora + "' data-_tipo='" + x._tipo + "'><b>" +
+                //x.Tipo + "</b><br><p>" + x.TipoDescricao.Substring(0, 43) + "...</p>
+                //</a></div><hr>";
+                // });
+                // }
+                // popover = popover + "<p style='text-align:center'><a href='" +
+                //Url.Action("Index", "Notificacoes") + "'>Ver todas notificações</a>";
+                // Session["ListaNotificacoes"] = popover;
+            }
+            catch (Exception)
+            {
+                //TODO
+            }
+            SetUsuarioSession(u);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout()
+        {
+            FinalizaSessao();
+            return View("Acesso");
+        }
+
+    }
+}
---
+++ questionario/SQ.Web/Filter/LoginFilter.cs
@@ -0,0 +1,43 @@
+﻿using SQ.Web.Controllers.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SQ.Web.Filter
+{
+    public class LoginFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = filterContext.HttpContext.Session.Contents["Usuario"];
+            if (session == null && !(filterContext.Controller is LoginController))
+            {
+                bool xmlRequest = filterContext.HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest";
+                if (!xmlRequest)
+                {
+                    filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary{
+                        {"controller" ,"Login"},
+                        {"action", "SessaoError"}
+                    }
+                );
+                }
+                else
+                {
+                    filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary{
+                        {"controller" ,"Login"},
+                        {"action", "SessaoErrorAjax"}
+                    }
+                );
+                }
+
+
+            }
+        }
+    }
+}
