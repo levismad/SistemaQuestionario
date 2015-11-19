8ea67e41410ac60154405043892ae248c401f679
Levi E <levi.esteves@gmail.com>
2015-11-19 17:34:21 -0200

    Adicionado DAO e Model Usuario

---
+++ questionario/SQ.Dominio/Interface/MUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQ.Dominio.Interface
+{
+    public class MUsuario
+    {
+        public string ebusiness { get; set; }
+        public string senha { get; set; }
+        public DateTime? DataHoraUltimoLogin { get; set; }
+    }
+}
---
+++ questionario/SQ.Repositorio/Interface/DUsuario.cs
@@ -0,0 +1,31 @@
+﻿using SQ.Dominio.Interface;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQ.Repositorio.Interface
+{
+    public class DUsuario
+    {
+        public MUsuario ConsultarUsuario(string business, string senha)
+        {
+            var usuarioValidos = ConfigurationManager.AppSettings["UsuariosLiberados"];
+            var idUsuario = usuarioValidos.IndexOf(business.ToLower());
+            if (idUsuario == -1)
+            {
+                throw new Exception("Login Inválido");
+            }
+
+            MUsuario usuario = new MUsuario();
+            usuario.DataHoraUltimoLogin = DateTime.Now;
+            usuario.ebusiness = usuarioValidos[idUsuario].ToString();
+            usuario.senha = "";
+
+            return usuario;
+
+        }
+    }
+}
