fdc0e34e065586fe8f0f749071dd7fa904e877e9
Levi E <levi.esteves@gmail.com>
2015-11-19 14:34:03 -0200

    Montada estrutura do projeto

---
+++ questionario/SQ.Core/Enums/DataKeyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class DataKeyAttribute : Attribute
+    {
+        public string DataKey { get; set; }
+
+        public DataKeyAttribute(string dataKey)
+        {
+            DataKey = dataKey;
+        }
+    }
+}
