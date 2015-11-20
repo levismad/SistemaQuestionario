b577fc155add50eb1935ce8caf829f36d2805190
Levi E <levi.esteves@gmail.com>
2015-11-20 15:28:11 -0200

    Adicionada Home Page

---
+++ questionario/SQ.Web/Controllers/Home/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using SQ.Web.Controllers.Base;
+using SQ.Web.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SQ.Web.Controllers.Home
+{
+    [LoginFilter]
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
